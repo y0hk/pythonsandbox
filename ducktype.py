8eb889cda544baf92339f0278e30b5e4dba20490
y0hk <y0hk1a@gmail.com>
2016-04-09 08:45:05 +0000

    ducktyping

---
+++ ducktype.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class SuperClass(object):
+    """スーパークラスに見立てたクラス"""
+
+    def greet(self):
+        """サブクラスでオーバーライドするインターフェース"""
+        print('Super')
+
+
+class DuckSubClass(object):
+    """ダックタイピングでSuperClassとして振る舞う"""
+
+    def greet(self):
+        print('Sub')
+
+
+def main():
+    # DuckSubClassはSuperClassのように振る舞う
+    # しかし、継承関係ではないのでisinstance()はFalse
+    obj = DuckSubClass()
+    print(isinstance(obj, SuperClass))
+
+
+if __name__ == '__main__':
+    main()
+
