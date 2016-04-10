105ddb3ad476a98a7d9cebdf05431d0c89c05b3c
y0hk <y0hk1a@gmail.com>
2016-04-10 19:45:52 +0000

    packaging

--- setup.py
+++ setup.py
@@ -9,13 +9,20 @@ from setuptools import find_packages
 def main():
     setup(
         name='package101',
-        version='0.0.1',
+        version='0.0.3',
         description='My first package',
         author='your name',
         author_email='email@example.jp',
-        packages=find_packages()
+        packages=find_packages(),
+        install_requires=[
+            'requests'
+            ],
+        entry_point={
+            'console_scripts': [
+                'greet = package101:greet'
+                ]
+            }
         )
-
 
 
 if __name__ == '__main__':
